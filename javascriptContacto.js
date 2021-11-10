be463fc7673053f69759087872a961c029da19d2
coti8855 <coti8855@gmail.com>
2021-11-10 00:10:51 -0300

    agregue cosas

--- javascriptContacto.js
+++ javascriptContacto.js
@@ -1,36 +1,24 @@
 
-let nombreUsuario = document.getElementById("nombreUsuario")
-  let apellidoUsuario = document.getElementById("apellidoUsuario")
-  let mailUsuario = document.getElementById("mailUsuario")
-  let texto = document.getElementById("texto")
-
-  let array =[]
-  console.log (array)
+$("#boton").on("click", function(e){
   
-function formulario (e){
-  
- e.preventDefault(e)
-  if ( nombreUsuario.value != "" && apellidoUsuario.value != "" && mailUsuario.value != "" && texto.value != ""){
+  let nombre = $("#nombreUsuario").val()
+  let apellido = $("#apellidoUsuario").val()
+  let mail = $ ("#mailUsuario").val()
+  if(nombre != "" && apellido != "" && mail != ""){
+    mostrar()
 
-    console.log("Bienvenido " + nombreUsuario.value)
-    let parrafo1 = document.createElement ("p")
-  let cartel = document.getElementById ("cartel")
-    parrafo1.innerHTML = ("Bienvenido/a " + nombreUsuario.value)
-    cartel.appendChild (parrafo1)
-    array.push({ nombre: nombreUsuario.value , apellido: apellidoUsuario.value , mail: mailUsuario.value , texto: texto.value})
-    let arrayJSON = JSON.stringify (array)
-    localStorage.setItem ("Usuarios", arrayJSON )
     
+  }else{
+    $(".error").html(`<h6>ERROR, complete los casilleros correspondientes</h6>`).css("color", "red")
+  }
+  e.preventDefault(e)
+})
 
-} else if (nombreUsuario.value == "" || apellidoUsuario.value == "" || mailUsuario.value == "" || texto.value == "") {
-   let parrafoNuevo = document.createElement ("p")
-   let cartel = document.getElementById ("cartel")
-    parrafoNuevo.innerHTML = ("Error")
-    cartel.appendChild (parrafoNuevo)
+function mostrar (){
+  let nombre = $("#nombreUsuario").val()
+   $("#cartel").html(`<div class="formulario">
+                       <h1>!Bienvenido ${nombre}!</h1>
+                       <p>¡Gracias por comunicarte!</p>
+                       </div>`).css("text-align", "right")
 }
 
-
-}
-
-let boton1 = document.getElementById ("boton")
-boton1.addEventListener ("click", formulario)
